53e43287a62ce1034330af859073cf1ef837fb71
森史憲 <mori@codingdesing.jp>
2024-04-05 13:55:20 +0900

    スタイルの確認用にlayouts/default.vueとpages/index.vueを追加＆app.vueを調整

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <header class="article">
+      <h1 class="article__title">トップページ</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt nostrum corrupti aspernatur repellendus enim a nesciunt quis? Ad repudiandae ducimus quisquam labore tempora reiciendis, ex omnis itaque deleniti, maiores quia.</p>
+      <p><a href="dummy" class="hover-opacity" style="border-radius: 10px; background-color: pink; padding: 10px; display: inline-flex;">テスト</a></p>
+    </header>
+  </article>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+.article {
+  &__title {
+    font-size: 2rem;
+    color: #333;
+  }
+}
+</style>
