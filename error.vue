8541a76db9406c8c96de9068f7e3e4c28cece1ca
森史憲 <mori@codingdesing.jp>
2024-04-11 16:13:03 +0900

    エラーページ　フッター表示追加

---
+++ error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="enclosure">
+    <div class="content">
+      <p class="content__title">404 Not Found</p>
+      <p class="content__description">お探しのページは見つかりません。</p>
+    </div>
+  </div>
+  <MoleculesCommonFooter />
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.content {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 188px 96px;
+
+  &__title {
+    font-size: 36px;
+    font-weight: bold;
+    @include fontTsuku;
+  }
+
+  &__description {
+    margin-top: 32px;
+  }
+}
+</style>
