5575b5383bbb5f7e5994046e5b486df1db087e9b
森史憲 <mori@codingdesing.jp>
2024-04-11 16:22:25 +0900

    デフォルトレイアウト　もろもろ調整
    - HTMLにクラスを反映
    - フッターを表示するように調整
    - style要素内にスタイルを追加＆scssのスタイルに調整

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,98 @@
 <template>
-  <div>
-    <slot />
+  <div class="enclosure">
+    <div class="nuxt-container">
+      <slot />
+    </div>
+
+    <MoleculesCommonFooter />
+
+    <!-- <common-header /> -->
+
+    <!-- <comp /> -->
   </div>
 </template>
+
+<script>
+import variables from '~/variables'
+import comp from '~/components/organisms/comp'
+// import commonHeader from '@/components/molecules/commonHeader'
+
+export default {
+
+}
+
+// export default {
+//   components: { comp, commonHeader, commonFooter },
+//   data() {
+//     return {
+//       bodyScale: 'none',
+//     }
+//   },
+//   watch: {
+//     '$store.state.isOpenModal'(val) {
+//       document.body.style.transform = val ? 'none' : this.bodyScale
+//     },
+//   },
+//   mounted() {
+//     this.onResize()
+//     this.onScroll()
+//     window.addEventListener('resize', this.onResize)
+//     window.addEventListener('scroll', this.onScroll)
+//   },
+//   methods: {
+//     onScroll() {
+//       if (this.$store.state.isOpenModal) {
+//         return
+//       }
+
+//       const windowWidth = window.innerWidth
+//       if (
+//         windowWidth >= parseInt(variables['breakpoint-1']) &&
+//         windowWidth < parseInt(variables['breakpoint-3'])
+//       ) {
+//         const scale = windowWidth / parseInt(variables['breakpoint-3'])
+//         document.body.style.height = `${this.$el.clientHeight * scale}px`
+//       } else {
+//         document.body.style.height = 'auto'
+//       }
+//     },
+//     onResize() {
+//       // TODOlater check other browser
+//       const windowWidth = window.innerWidth
+//       if (
+//         windowWidth >= parseInt(variables['breakpoint-1']) &&
+//         windowWidth < parseInt(variables['breakpoint-3'])
+//       ) {
+//         this.bodyScale = `scale(${
+//           windowWidth / parseInt(variables['breakpoint-3'])
+//         })`
+//       } else {
+//         this.bodyScale = 'none'
+//       }
+
+//       if (!this.$store.state.isOpenModal) {
+//         document.body.style.transform = this.bodyScale
+//       }
+
+//       this.$store.commit(
+//         'updateIsSmartphone',
+//         windowWidth < parseInt(variables['breakpoint-1'])
+//       )
+//     },
+//   },
+// }
+</script>
+
+<style scoped lang="scss">
+.enclosure {
+  position: relative;
+  min-width: $width-2;
+  min-height: 100vh;
+}
+
+@include smartphone {
+  .enclosure {
+    min-width: initial;
+  }
+}
+</style>
